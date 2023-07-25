4cc1f6f82a847d129a6d0999b4f8199c55adf72c
Mayen Kalu <kalumayen@gmail.com>
2023-07-25 22:50:22 +0100

    feat(project): modify webpack configuration

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,19 +1,28 @@
-import '../css/main.css';
-const $ = require('jquery');
-const _ = require('lodash');
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
 
-let counter = 0;
+$(document).ready(() => {
+    const $body = $('body');
 
-$("<div id='logo'> </div>").appendTo('body');
-$("<p>Holberton Dashboard</p>").appendTo('body');
-$("<p>Dashboard data for the students</p>").appendTo('body');
-$("<button>Click here to get started</button>").appendTo('body');
-$("<p id='count'></p>").appendTo('body');
-$("<p>Copyright - Holberton School</p>").appendTo('body');
+    $body.append(
+        $('<p>').text('Holberton Dashboard'),
+        $('<p>').text('Dashboard data for the students'),
+        $('<button>').text('Click here to get started'),
+        $('<p>').attr('id', 'count'),
+        $('<p>').text('Copyright - Holberton School')
+    );
 
-function updateCounter() {
-  counter++;
-  $('#count').html(`${counter} clicks on the button`);
-}
+    let clickcount = 0; //counter to track number of clicks
 
-$("button").on("click", _.debounce(updateCounter, 500));
+    //function to update counter and show count
+    function updateCounter() {
+        clickcount++;
+        $('#count').text(`${clickcount} clicks on the button`);
+    }
+
+    //debounce to update the counter and prevent spammers
+    const debouncedUpdateCounter = debounce(updateCounter, 500);
+
+    //to bind the function above to the click button
+    $body.on('click', 'button', debouncedUpdateCounter);
+});
